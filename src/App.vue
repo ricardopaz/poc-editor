b9cc59d30a8bb983456a8db9a1ce9f6dfc926689
Ricardo Paz dos Santos Filho <ricardo.paz@192.168.1.5>
2022-03-11 10:46:05 -0300

    Using Quill plugin and some modules to create a text editor.

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <TextEditor />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TextEditor from './components/TextEditor.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TextEditor
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
-import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import { createApp } from 'vue'
+
+const app = createApp(App)
+app.mount('#app')
